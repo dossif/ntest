cf2681490885bbfbf6e1f0dffdd279c662e9289a
dossif <dossif@dossif.net>
2025-01-06 17:20:54 +0700

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 APP="ntest"
 VER="1.0.2"
 go build -v -a -trimpath -ldflags "-X main.appVersion=${VER}" -o $APP ./cmd/ntest
-cp -f ./$APP /usr/local/bin
-gsudo chown root:staff /usr/local/bin/$APP
-gsudo chmod u+s /usr/local/bin/$APP
+sudo cp -f ./$APP /usr/local/bin
+sudo chown root:staff /usr/local/bin/$APP
+sudo chmod u+s /usr/local/bin/$APP
 rm ./ntest
